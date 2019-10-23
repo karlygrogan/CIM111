1bbb92408e2b34e248196b80b70c856b37e55fa8
Karly Grogan <ksg55@miami.edu>
2019-10-23 13:10:29 -0400

    fixed image

---
+++ class_examples/js_intro/script.js
@@ -0,0 +1,18 @@
+//var array1 = [1, 2, 3, 4, 5, 6];
+//var array2= ["This is a string", 'This is also a string'];
+//var x = 4;
+//var heading = document.getElementsByTagName("h1");
+//console.log(heading);
+//console.log (x);
+//console.log (document.getElementsByTagName("h1"));
+
+$(document).ready(function () {
+    var x= 2+ 2;
+    console.log(x);
+    var y = 2 + "2";
+    console.log(y);
+    var z;
+    console.log(z);
+    //console.log("Everything loaded!");
+    //console.log("this function ran");
+});
